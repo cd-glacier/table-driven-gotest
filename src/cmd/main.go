2765477edc986d2de09c17416dbc64fe3aae3bc7
g-hyoga <hyoga0216@gmail.com>
2018-10-14 16:47:51 +0900

    Parse flag

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+
+	"github.com/k0kubun/pp"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	packageName := *flag.String("package", "./src/cmd/", "package name")
+	testFileName := *flag.String("file", "main_test.go", "test file name")
+	testFnName := *flag.String("function", "TestMain", "test function name")
+	testCaseNum := *flag.Int("testcase", 0, "test case number: start 0")
+	flag.Parse()
+
+	pp.Println(packageName, testFileName, testFnName, testCaseNum)
 }
